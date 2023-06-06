b6d5f36f2f31e9381ed4728537b710ccc22e2896
sirojiddin005 <sirojiddin.05@bk.ru>
2023-06-06 14:29:28 +0500

    readme file changed

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import './App.css';
+import img1 from './img1.svg';
+import img2 from './img2.svg';
+import wix from './wix.svg';
+
+function App() {
+  return (
+    <div> 
+      <div className="img"></div>
+     <header>
+      <div className="div1">
+        <div className="items">
+          <a href=""><img className="wix" src={wix} alt="" /></a>
+          <button className="b1"><img src={img1} alt="" /></button>
+          <hr />
+          <button className="b2"><img src={img2} alt="" /></button>
+          </div>
+          <div className="items2">
+          <p>Click edit and create your own amazing website</p>
+          <a href="">Подробнее</a>
+          <a href="" className="b3">Редактировать</a>
+          </div>
+      </div>
+     </header> 
+     <section className="main1">
+      
+        <div className="menu">
+          <a href=""><p>K. Griffith</p></a>
+          <div class="span">
+          <span></span>
+          <span></span>
+          <span></span>
+          </div>
+          <ul>
+            <li><a href="">APPEARANCES</a></li>
+            <li><a href="">BOOKS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">CONTACT</a></li>
+          </ul>
+        </div>
+     <div className="author">
+      <ul>
+        <li><a href=""><img src="https://static.wixstatic.com/media/c4392d634a0148fda8b7b2b0ad98293b.png/v1/fill/w_25,h_25,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c4392d634a0148fda8b7b2b0ad98293b.png" alt="" /></a></li>
+
+        <li><a href=""><img src="https://static.wixstatic.com/media/e0678ef25486466ba65ef6ad47b559e1.png/v1/fill/w_25,h_25,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e0678ef25486466ba65ef6ad47b559e1.png" alt="" /></a></li>
+      </ul>
+      <img className="woman" src="https://static.wixstatic.com/media/ad420a_ca05081d4ece492fb6589c81baee1091~mv2.jpg/v1/crop/x_1416,y_0,w_3091,h_3414/fill/w_238,h_263,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-11967148821.jpg" alt="" />
+      <div>
+      <h1>Kayla Griffith</h1>
+      <p>Award Winning Author</p>
+      </div>
+     </div>
+    </section>
+
+    <div className="main2">
+      <div className="center">    
+        <div id="comp-kk2mpwaj3"><div data-testid="svgRoot-comp-kk2mpwaj3" class="aizuI7 TcoJIb comp-kk2mpwaj3"><svg preserveAspectRatio="xMidYMid meet" data-bbox="15 87.5 170 25" viewBox="15 81.5 170 1" height="100" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true" aria-labelledby="svgcid--w3pcgs7n500k"><title id="svgcid--w3pcgs7n500k"></title>
+    <g>
+        <path d="M185 87.5l-170 .003V112.5h170l-10.339-12.502L185 87.5z"></path>
+    </g>
+</svg>
+  <p className="p1">New Release</p>
+     <div className="swan">
+      <h1>The Swan Isle 
+        <br/> (2035)</h1>
+        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me tadd your own content and make changes to the font. I’m a great place for you to tell a story and let your users know little more about you.</p>
+        <h2>Order Now</h2>
+        <a href="">Amazon</a>
+        <a href="">Google</a>
+        <a href="">ibooks</a>
+        </div>
+     </div>      
+    </div>   
+       <img src="https://static.wixstatic.com/media/ad420a_ac6bbfd46dbe4c83a234221d29d67791~mv2.png/v1/fill/w_385,h_543,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screen%20Shot%202021-02-07%20at%209_24_47.png" alt="" />    
+      </div>
+      </div> 
+
+      <div className="main3">
+      <div id="vec" ><div data-testid="svgRoot-comp-kkqltjgu" class="aizuI7 TcoJIb comp-kkqltjgu">
+        <svg preserveAspectRatio="xMidYMid meet" data-bbox="156.9 23 261.9 506.7" viewBox="156.9 23 261.9 506.7" height="60" width="30"         xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true"         aria-labelledby="svgcid-epyaxi4kqwd8"><defs></defs><title id="svgcid-epyaxi4kqwd8"></title>
+    <g>
+        <path fill="#ef7d5f" d="M287.8 398.7l131 131V23H156.9v506.7l130.9-131z" data-color="1"></path>
+    </g>
+      </svg>
+     </div>
+    </div>
+       <h1>Praise & Reviews</h1>
+       <div className="text">
+      <div>
+      <svg className="icon" preserveAspectRatio="xMidYMid meet" data-bbox="30.5 43.999 139 112" viewBox="30.5 43.999 139 112" height="30" width="50" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true" aria-labelledby="svgcid-ke65lulsxuuo"><title id="svgcid-ke65lulsxuuo"></title>
+    <g><path d="M84.463 44.235c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.995-22.337 1.456-41.414 16.577-57.994 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105h.416z"></path><path d="M162.148 44c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.994-22.338 1.457-41.415 16.577-57.995 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105l.416.001z"></path></g></svg>
+        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        <p>‘Swan Isle’ is Griffith's best writing yet” The Times Book Review <hr /></p>
+      </div>
+      <div>
+      <svg className="icon" preserveAspectRatio="xMidYMid meet" data-bbox="30.5 43.999 139 112" viewBox="30.5 43.999 139 112" height="30" width="50" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true" aria-labelledby="svgcid-ke65lulsxuuo"><title id="svgcid-ke65lulsxuuo"></title>
+    <g><path d="M84.463 44.235c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.995-22.337 1.456-41.414 16.577-57.994 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105h.416z"></path><path d="M162.148 44c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.994-22.338 1.457-41.415 16.577-57.995 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105l.416.001z"></path></g></svg>
+       <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+       <p>‘Gripping storytelling’The Good Read Blog<br /><br /> <hr /></p>
+      </div>
+      <div>
+      <svg className="icon" preserveAspectRatio="xMidYMid meet" data-bbox="30.5 43.999 139 112" viewBox="30.5 43.999 139 112" height="30" width="50" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true" aria-labelledby="svgcid-ke65lulsxuuo"><title id="svgcid-ke65lulsxuuo"></title>
+    <g><path d="M84.463 44.235c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.995-22.337 1.456-41.414 16.577-57.994 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105h.416z"></path><path d="M162.148 44c2.428 4.024 4.786 8.047 7.352 12.348-1.179.694-2.358 1.457-3.537 2.081-9.294 5.411-18.242 11.169-25.525 19.355-7.422 8.325-11.028 18.037-12.069 29.344.971 0 1.942-.069 2.913 0 5.757.416 11.653.139 17.202 1.457 13.109 3.052 18.103 13.111 17.202 25.39-.902 11.724-10.612 21.159-22.126 21.713-8.462.416-16.577-.763-23.236-6.59-6.034-5.272-8.74-12.418-10.474-19.979-4.994-22.338 1.457-41.415 16.577-57.995 7.63-8.325 16.508-15.123 26.08-21.02 3.121-1.942 6.104-4.024 9.225-6.105l.416.001z"></path></g></svg>
+        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        <p>Grifith’s voice tells the story of all women “The Seattle Post Review”  <hr /></p>
+        </div>
+       </div>
+      </div> 
+
+      <div className="main4">
+        <h1>See Upcoming Appearances</h1>
+        <p>I'm a paragraph. Click here to add your own text and edit me. It's easy.</p>
+
+        <div className="club">
+          <h2>January 18th 2035, The Breakfast Club, Virtual Book Reading of Swan Isle, 7PM - 8PM EST</h2>
+          <a href="">Join</a>
+        </div>
+        <div className="club">
+          <h2>January 31st 2035, The Good Read Club, Online Conversation with Kayla Griffith, 8PM - 9PM EST</h2>
+          <a href="">Join</a>
+        </div>
+        <div className="club">
+          <h2>March 2nd 2035, Otto Cafe, Online Book Reading With Kayla Griffith, 3PM - 4PM EST</h2>
+          <a href="">Join</a>
+        </div>
+        <div className="kayla">
+        <div id="vec" ><div data-testid="svgRoot-comp-kkqltjgu" class="aizuI7 TcoJIb comp-kkqltjgu">
+        <svg preserveAspectRatio="xMidYMid meet" data-bbox="156.9 23 261.9 506.7" viewBox="156.9 23 261.9 506.7" height="60" width="30"         xmlns="http://www.w3.org/2000/svg" data-type="color" role="presentation" aria-hidden="true"         aria-labelledby="svgcid-epyaxi4kqwd8"><defs></defs><title id="svgcid-epyaxi4kqwd8"></title>
+      <g>
+        <path fill="#ef7d5f" d="M287.8 398.7l131 131V23H156.9v506.7l130.9-131z" data-color="1"></path>
+      </g>
+      </svg>
+     </div>
+    </div>
+    <img src="https://static.wixstatic.com/media/ad420a_3b0ac18b8f8c44e6968cf137c044b3b4~mv2.jpg/v1/crop/x_1678,y_0,w_3414,h_3414/fill/w_177,h_177,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/GettyImages-11967148791.jpg" alt="" />
+    <h2>About Kayla Griffith</h2>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    <a href="">Read More</a>
+        </div>
+      </div>
+      <footer>
+        <p>© 2035 by K.Griffith. Powered and secured by <a href="">Wix</a></p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
